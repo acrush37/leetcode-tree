911ecc72ebf8a9cd6f92f1dfa82085869db9a642
acrush37 <acrush37@gmail.com>
2019-12-04 17:13:31 +0800

    LeetCode Tree

---
+++ src/medium/FlipEquivalentBinaryTrees.java
@@ -0,0 +1,35 @@
+package medium;
+
+/*
+    For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+    A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+    Write a function that determines whether two binary trees are flip equivalent.  The trees are given by root nodes root1 and root2.
+ */
+public class FlipEquivalentBinaryTrees {
+
+    public static void main(String... args) {
+
+        FlipEquivalentBinaryTrees flipEquivalentBinaryTrees = new FlipEquivalentBinaryTrees();
+        System.out.println(flipEquivalentBinaryTrees.flipEquiv(null, null));
+    }
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+
+        if (root1 == null) return root2 == null;
+        if (root2 == null) return root1 == null;
+        if (root1.val != root2.val) return false;
+        return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
+                || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+    }
+
+}
